2873deb9551bec122de09ed5f6301afd24a4de6f
Gustavo <gustavocaraujo@gmail.com>
2017-07-01 16:07:40 -0300

    added routes

--- 06-express/app/index.js
+++ 06-express/app/index.js
@@ -1,3 +1,23 @@
 const {host, port} = require('./config.js')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+//setup
+const app = express()
+app.use(bodyParser.json())
+
+//publish HTML5 app
+app.get('/', (request, response) => {
+    response.send("Hi")
+})
+
+app.get('/about', (request, response) => {
+    response.send('This is a express application')
+})
+
+//server startup
+app.listen(port, () => {
+    console.log(`Server starter at http://${host}:${port}`)
+})
 
 console.log(host, port)
