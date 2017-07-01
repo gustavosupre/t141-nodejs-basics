3725522271127ad2e785ced1fcf7f247c6470c8f
Gustavo <gustavocaraujo@gmail.com>
2017-07-01 12:22:12 -0300

    comando de status para gitignore

--- 03-npm/index.js
+++ 03-npm/index.js
@@ -1,6 +1,7 @@
 //criar o package JSON via git BASH: $ npm init
 // instalar package npm do site via git BASH: $ npm install "nome do modulo" --save
 //file .gitignore na raiz - para nao commitar alguma pasta
+    //dar um $git status e ver a pasta a ser ignorada no commit na lista dos arquivos
 
 //iniciando o arquivo
 
